cf54273f76d4f67f31ad3800d4a9b6ccf828c452
Thakur Shivam Kumar <shivamthakur.ss129@gmail.com>
2025-08-14 11:39:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function playMusic(songid){
-    let music = document.getElementById(songid)
+let music = null;
+let last_music = null
+
+function playMusic(songid, songname){
+    music = document.getElementById(songid)
+    try{
+        last_music.pause()
+    }
+    catch{
+        //not do anything
+    }
+    
     music.play()
+    last_music = music
+    document.getElementById('sname').innerHTML = songname
+    document.getElementById('player-btn').src = "pause.png"
 }
+
+function pauseMusic(){
+    music.pause()
+    document.getElementById('player-btn').src = "play-button.png"
+    document.getElementById('sname').innerHTML = "Your song name appears here"
+}
+
+
